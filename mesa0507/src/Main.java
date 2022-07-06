6a975973b25d1fa7e69301b89ef6c090f14ffaa6
geyson_k <89105671+geysonk@users.noreply.github.com>
2022-07-05 21:04:58 -0300

    Correcao

--- mesa0507/src/Main.java
+++ mesa0507/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Licitante licitante = new Licitante("Joao", "Silva", 123456789, 100.0);
+        Licitante licitante = new Licitante("Jose", "Joao", 230223022302, 100.0);
         Leilao leilao = new Leilao(90.0, "Leilão de carros");
-        Licitante licitante2 = new Licitante("Maria", "Santos", 987654321, 80.0);
+        Licitante licitante2 = new Licitante("Joao", "Jose", 022302230223, 80.0);
         leilao.adicionarObservador(licitante);
         leilao.adicionarObservador(licitante2);
         leilao.notificar();
